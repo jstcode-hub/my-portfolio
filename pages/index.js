f4a3243f954f0a4a87e129858d65beadcd47497b
jstcode-hub <jstcode.hub@gmail.com>
2022-11-27 21:54:21 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -27,27 +27,29 @@ export default function Home() {
       </Head>
 
       <main className="bg-white px-8 md:px-20 lg:px-40 dark:bg-gray-900">
-        <section className=" min-h-screen">
-          <nav className="py-10 px-5 mb-8 flex justify-between">
-            <h1 className="text-xl font-burtons dark:text-white">JstCode.Hub</h1>
+        <section className="min-h-screen">
+          <nav className="py-5 px-16 mb-8 flex justify-between fixed top-0 left-0 right-0 bg-teal-600">
+            <a href="#" className="text-xl font-burtons dark:text-white">
+              JstCode.Hub
+            </a>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl" />
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-white" />
               </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white   px-4 py-2 rounded-md ml-4 md:ml-8" href="#">
-                  Resume
+                <a href="#portofolio" className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white   px-4 py-2 rounded-md ml-4 md:ml-8">
+                  Portofolio
                 </a>
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10 ">
+          <div className="text-center p-10 mt-10">
             <h2 className="text-5xl text-teal-600 font-medium md:text-6xl ">Moh. Angga Yunus</h2>
             <h3 className="text-2xl pt-4 md:text-3xl dark:text-white">Javascript Developer</h3>
             <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white">Seorang manusia yang mencari nikmat duniawi agar bisa hidup bahagia</p>
           </div>
           <div>
-            <ul className="text-5xl flex justify-center gap-4 md:gap-16 py-3 text-gray-600">
+            <ul className="text-5xl flex justify-center gap-4 md:gap-16 py-3 text-gray-600 dark:text-white">
               <li>
                 <a href="https://www.instagram.com/jstkidng_/">
                   <AiFillInstagram />
@@ -82,49 +84,61 @@ export default function Home() {
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">Sekarang masih mencari free job dan sedang mencari universitas</p>
           </div>
-          <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+          <div className="lg:flex gap-10 dark:text-white">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-600">
               <Image src={design} alt="Design" width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful designs</h3>
-              <p className="py-2">Creating elegant designs suited for your needs following core design theory</p>
-              <h4 className="py-4 text-teal-600 font-medium">Design tools I use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
+              <h3 className="text-lg font-medium pt-8 pb-2">Design Yang Indah</h3>
+              <p className="py-2">Membuat sebuah design yang merepesentasikan tentang diri dan produk anda ke khalayak ramai</p>
+              <h4 className="py-4 text-teal-600 dark:text-white font-medium">Beberapa tools yang saya gunakan</h4>
+              <div className="text-gray-800 dark:text-white ">
+                <p className="py-1">Photoshop</p>
+                <p className="py-1">Illustrator</p>
+                <p className="py-1">Figma</p>
+              </div>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-600">
               <Image src={code} alt="Code" width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2">Elegant Code</h3>
-              <p className="py-2">Programs must be written for people to read, and only incidentally for machines to execute.</p>
-              <h4 className="py-4 text-teal-600 font-medium">Coding tools I use</h4>
-              <p className="text-gray-800 py-1">Visual Studio Code</p>
-              <p className="text-gray-800 py-1">Android Studio</p>
-              <p className="text-gray-800 py-1">Visual Studio</p>
+              <h3 className="text-lg font-medium pt-8 pb-2">Kodingan Yang Elegant</h3>
+              <p className="py-2">Menulis program yang elegant, rapih, dan mudah di baca serta teknologi yang up to date</p>
+              <h4 className="py-4 text-teal-600 dark:text-white font-medium">Beberapa tools yang saya gunakan</h4>
+              <div className="text-gray-800 dark:text-white">
+                <p className=" py-1">Visual Studio Code</p>
+                <p className="py-1">Android Studio</p>
+                <p className="py-1">Visual Studio</p>
+              </div>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-600">
               <Image src={consulting} alt="Consulting" width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2">Consultant</h3>
-              <p className="py-2">My greatest strenght as a consultant is to be ignorant and ask a few questions.</p>
-              <h4 className="py-4 text-teal-600 font-medium">Consultant tools I use</h4>
-              <p className="text-gray-800 py-1">Google Meet</p>
-              <p className="text-gray-800 py-1">Zoom Meet</p>
-              <p className="text-gray-800 py-1">Notion</p>
+              <h3 className="text-lg font-medium pt-8 pb-2">Konsultasi</h3>
+              <p className="py-2">Membantu memecahkan masalah yang sedang kalian hadapi di perjalan karir anda</p>
+              <h4 className="py-4 text-teal-600 dark:text-white font-medium">Beberapa tools yang saya gunakan</h4>
+              <div className="text-gray-800 dark:text-white">
+                <p className="py-1">Google Meet</p>
+                <p className="py-1">Zoom Meet</p>
+                <p className="py-1">Notion</p>
+              </div>
             </div>
           </div>
         </section>
-        <section>
+        <section id="portofolio">
           <div className="text-center">
-            <h3 className="text-3xl py-1 dark:text-white">My Portofolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">These are some of the projects I`ve created</p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">I offer a wide range of service, including brand design, programming and teaching</p>
+            <h3 className="text-3xl py-1 dark:text-white">Kumpulan Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">Kumpulan beberapa portofolio maupun tugas yang pernah saya buat</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+              Beberapa portofolio ini saya buat dengan dengan tujuan unutuk memperlihatkan seberapa jauh pengalaman ataupun keahlian yang sudah saya capai
+            </p>
           </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1">
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap dark:text-white">
+            <div className="basis-1/3 flex-1 rounded-2xl p-3 shadow-2xl dark:bg-gray-600">
               <a href="https://axifiveth.vercel.app/" target="_blank" rel="noopener noreferrer">
                 <Image src={web1} alt="" className="rounded-lg object-cover" width={'100%'} height={'100%'} layout={'responsive'} />
               </a>
-              <p className="my-3 p-3 shadow-lg rounded-xl dark:bg-white ">
-                Project pertama saya yang berhasil online. Project tersebut saya membuatnya dengan teknologi NEXTJS dan firebase dengan fitur lengkap CRUD dan login menggunakan Google
+              <p className="my-3 p-3">
+                Ini meupakan sebuah tugas saya untuk tugas akhir sekolah. Ini merupakan aplikasi chatting bersama untuk membantu kalian mencari teman baru. Teknologi yang saya gunakan meliputi Next.js dan
+                Firebase dengan sistem auth google di dalamnya. Anda dapat mengunjungi halaman tersebut di link berikut ini :{' '}
+                <a href="https://axifiveth.vercel.app/" target="_blank" rel="noopener noreferrer">
+                  https://axifiveth.vercel.app
+                </a>
               </p>
             </div>
             <div className="basis-1/3 flex-1">
